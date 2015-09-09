53d84d3e7e6e9a1fcd2b4a0cd03a55517e5df1be
uhyo <uhyo@uhyohyo.net>
2015-09-09 23:44:31 +0900

    save/testplay ui

--- jsx/core/index.jsx
+++ jsx/core/index.jsx
@@ -1,6 +1,7 @@
 var React=require('react'),
     Reflux=require('reflux');
 
+
 var mapStore=require('../../stores/map'),
     paramStore=require('../../stores/params'),
     editStore=require('../../stores/edit');
@@ -10,14 +11,27 @@ var MapEdit=require('./map-edit.jsx'),
     EditMode=require('./edit-mode.jsx'),
     MiniMap=require('./mini-map.jsx'),
     ScreenSelect=require('./screen-select.jsx'),
-    ParamEdit=require('./param-edit.jsx');
+    ParamEdit=require('./param-edit.jsx'),
+    Button=require('./util/button.jsx');
+
 
 var MasaoEditorCore = React.createClass({
     displayName: "MasaoEditorCore",
     mixins:[Reflux.connect(mapStore,"map"), Reflux.connect(paramStore,"params"), Reflux.connect(editStore,"edit")],
     propTypes:{
         filename_pattern: React.PropTypes.string.isRequired,
-        filename_chips: React.PropTypes.string.isRequired
+        filename_chips: React.PropTypes.string.isRequired,
+
+        text_save: React.PropTypes.string,
+        text_testplay: React.PropTypes.string,
+        requestSave: React.PropTypes.func,
+        requestTestplay: React.PropTypes.func
+    },
+    getDefaultProps(){
+        return {
+            text_save: "保存",
+            text_testplay: "テストプレイ"
+        };
     },
     render(){
         var map=this.state.map, params=this.state.params, edit=this.state.edit;
@@ -28,9 +42,25 @@ var MasaoEditorCore = React.createClass({
         }else if(edit.screen==="params"){
             screen=<ParamScreen params={params} edit={edit}/>;
         }
+        var button_save=null;
+        if(this.props.requestSave!=null){
+            button_save=<div>
+                <Button label={this.props.text_save} onClick={this.handleSaveClick}/>
+            </div>;
+        }
+        var button_testplay=null;
+        if(this.props.requestTestplay!=null){
+            button_testplay=<div>
+                <Button label={this.props.text_testplay} onClick={this.handleSaveClick}/>
+            </div>;
+        }
         return <div className="me-core">
             <div className="me-core-info">
-                <ScreenSelect edit={edit}/>
+                <div>
+                    <ScreenSelect edit={edit}/>
+                </div>
+                {button_save}
+                {button_testplay}
             </div>
             {screen}
         </div>;
---
+++ jsx/core/util/button.jsx
@@ -0,0 +1,13 @@
+var React=require('react');
+
+module.exports = React.createClass({
+    displayName: "Button",
+    propTypes: {
+        label: React.PropTypes.string.isRequired,
+        onClick: React.PropTypes.func
+    },
+    render(){
+        return <div className="me-core-util-button" onClick={this.props.onClick}>{this.props.label}</div>;
+    }
+});
+
