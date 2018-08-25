7d9d15a6b965c1b2b44ba207c6c58f67f7f9865a
uhyo <uhyo@uhy.ooo>
2018-08-26 02:54:21 +0900

    extract runExternalCommand to new file

--- logics/command/index.ts
+++ logics/command/index.ts
@@ -1,9 +1,8 @@
 // command
 import editStore from '../../stores/edit';
-import commandStore from '../../stores/command';
-import { getCurrentGame } from '../game';
 import { runMapCommand } from './map-screen';
 import { Command, ExternalCommand, commandNames } from './def';
+import { runExternalCommand } from './external';
 
 export { Command, ExternalCommand, commandNames };
 
@@ -14,45 +13,8 @@ export { Command, ExternalCommand, commandNames };
 export function run(command: Command, keydown: boolean): boolean {
   if (keydown) {
     // external系コマンドを先に処理
-    switch (command) {
-      case 'external:save': {
-        commandStore.invokeCommand({
-          type: 'save',
-          kind: 'default',
-        });
-        return true;
-      }
-      case 'external:json': {
-        commandStore.invokeCommand({
-          type: 'save',
-          kind: 'json',
-        });
-        return true;
-      }
-      case 'external:html': {
-        commandStore.invokeCommand({
-          type: 'save',
-          kind: 'html',
-        });
-        return true;
-      }
-      case 'external:open': {
-        commandStore.invokeCommand({
-          type: 'open',
-        });
-        return true;
-      }
-      case 'external:testplay': {
-        // テストプレイを要求
-        const game = getCurrentGame();
-        const { stage } = editStore.state;
-        commandStore.invokeCommand({
-          type: 'testplay',
-          game,
-          stage,
-        });
-        return true;
-      }
+    if (runExternalCommand(command)) {
+      return true;
     }
   }
   const { screen } = editStore.state;
