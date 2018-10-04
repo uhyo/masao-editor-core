7d856d2930f5af71ccc039baee4489ab83f0e37c
uhyo <uhyo@uhy.ooo>
2018-10-04 20:51:49 +0900

    fix behavior of param-edit in non-fitting mode

--- jsx/core/screen/param-screen/param-edit.tsx
+++ jsx/core/screen/param-screen/param-edit.tsx
@@ -11,6 +11,7 @@ import { EditState, ParamsState, ProjectState } from '../../../../stores';
 import * as styles from '../../css/param-edit.css';
 import { FormField, FormControls } from '../../../components/form-controls';
 import { Toolbar, Toolbox } from '../../../components/toolbar';
+import { ThemeConsumer } from '../../theme/context';
 
 export interface IPropParamEdit {
   edit: EditState;
@@ -19,10 +20,13 @@ export interface IPropParamEdit {
 }
 
 export default class ParamEdit extends React.Component<IPropParamEdit, {}> {
+  private mainRef = React.createRef<HTMLDivElement>();
   componentDidUpdate(prevProps: IPropParamEdit) {
     if (prevProps.edit.param_type !== this.props.edit.param_type) {
-      let main = this.refs['main'] as HTMLElement;
-      main.scrollTop = 0;
+      const main = this.mainRef.current;
+      if (main != null) {
+        main.scrollTop = 0;
+      }
     }
   }
   render() {
@@ -62,158 +66,169 @@ export default class ParamEdit extends React.Component<IPropParamEdit, {}> {
     );
 
     return (
-      <>
-        {typeMenu}
-        <div ref="main" className={styles.main}>
-          <FormControls>
-            {(param_type === ''
-              ? keys
-              : masao.paramTypes[param_type].params
-            ).map(key => {
-              let description: string;
-              let field;
-              let obj: masao.param.Data;
-              if (/@@@/.test(key)) {
-                //色コントロールを設置
-                let key_red = key.replace('@@@', 'red'),
-                  key_green = key.replace('@@@', 'green'),
-                  key_blue = key.replace('@@@', 'blue');
-                obj = data[key_red];
-                const color = {
-                  red: Number(params[key_red]),
-                  green: Number(params[key_green]),
-                  blue: Number(params[key_blue]),
-                };
-                const onChange = ({
-                  red,
-                  green,
-                  blue,
-                }: {
-                  red: number;
-                  green: number;
-                  blue: number;
-                }) => {
-                  paramsLogic.changeParams({
-                    [key_red]: String(red),
-                    [key_green]: String(green),
-                    [key_blue]: String(blue),
-                  });
-                };
-                field = <Color value={color} onChange={onChange} />;
-                description = obj.description.replace(/（.+）$/, '');
-              } else {
-                obj = data[key];
-                description = obj.description;
-                //versionがあれか見る
-                let version = obj.version;
-                let pv: '2.8' | 'fx' | 'kani2' =
-                  project.version === 'fx16' ? 'fx' : project.version;
-                if (version && version[pv] === false) {
-                  //これは表示しない
-                  return null;
-                }
-                //typeに応じて
-                if (obj.type === 'enum') {
-                  let enumValues = obj.enumValues;
-                  if (version != null && Array.isArray(version[pv])) {
-                    //選択肢の制限
-                    enumValues = enumValues.filter(obj => {
-                      return (
-                        (version![pv] as Array<string>).indexOf(obj.value) >= 0
+      <ThemeConsumer>
+        {({ fitY }) => (
+          <>
+            {typeMenu}
+            <div
+              ref={this.mainRef}
+              className={`${styles.main} ${
+                fitY ? styles.mainFitting : styles.mainNonFitting
+              }`}
+            >
+              <FormControls>
+                {(param_type === ''
+                  ? keys
+                  : masao.paramTypes[param_type].params
+                ).map(key => {
+                  let description: string;
+                  let field;
+                  let obj: masao.param.Data;
+                  if (/@@@/.test(key)) {
+                    //色コントロールを設置
+                    let key_red = key.replace('@@@', 'red'),
+                      key_green = key.replace('@@@', 'green'),
+                      key_blue = key.replace('@@@', 'blue');
+                    obj = data[key_red];
+                    const color = {
+                      red: Number(params[key_red]),
+                      green: Number(params[key_green]),
+                      blue: Number(params[key_blue]),
+                    };
+                    const onChange = ({
+                      red,
+                      green,
+                      blue,
+                    }: {
+                      red: number;
+                      green: number;
+                      blue: number;
+                    }) => {
+                      paramsLogic.changeParams({
+                        [key_red]: String(red),
+                        [key_green]: String(green),
+                        [key_blue]: String(blue),
+                      });
+                    };
+                    field = <Color value={color} onChange={onChange} />;
+                    description = obj.description.replace(/（.+）$/, '');
+                  } else {
+                    obj = data[key];
+                    description = obj.description;
+                    //versionがあれか見る
+                    let version = obj.version;
+                    let pv: '2.8' | 'fx' | 'kani2' =
+                      project.version === 'fx16' ? 'fx' : project.version;
+                    if (version && version[pv] === false) {
+                      //これは表示しない
+                      return null;
+                    }
+                    //typeに応じて
+                    if (obj.type === 'enum') {
+                      let enumValues = obj.enumValues;
+                      if (version != null && Array.isArray(version[pv])) {
+                        //選択肢の制限
+                        enumValues = enumValues.filter(obj => {
+                          return (
+                            (version![pv] as Array<string>).indexOf(
+                              obj.value,
+                            ) >= 0
+                          );
+                        });
+                      }
+                      const fieldChange = (
+                        e: React.SyntheticEvent<HTMLSelectElement>,
+                      ) => {
+                        const value = e.currentTarget.value;
+                        paramsLogic.changeParam(key, value);
+                      };
+                      field = (
+                        <select value={params[key]} onChange={fieldChange}>
+                          {enumValues.map(obj => {
+                            return (
+                              <option key={obj.value} value={obj.value}>
+                                {obj.description}
+                              </option>
+                            );
+                          })}
+                        </select>
                       );
-                    });
-                  }
-                  const fieldChange = (
-                    e: React.SyntheticEvent<HTMLSelectElement>,
-                  ) => {
-                    const value = e.currentTarget.value;
-                    paramsLogic.changeParam(key, value);
-                  };
-                  field = (
-                    <select value={params[key]} onChange={fieldChange}>
-                      {enumValues.map(obj => {
-                        return (
-                          <option key={obj.value} value={obj.value}>
-                            {obj.description}
-                          </option>
+                    } else if (obj.type === 'boolean') {
+                      const checkChange = (
+                        e: React.SyntheticEvent<HTMLInputElement>,
+                      ) => {
+                        paramsLogic.changeParam(
+                          key,
+                          e.currentTarget.checked ? '1' : '2',
                         );
-                      })}
-                    </select>
+                      };
+                      field = (
+                        <input
+                          type="checkbox"
+                          checked={params[key] === '1'}
+                          onChange={checkChange}
+                        />
+                      );
+                    } else if (obj.type === 'boolean-reversed') {
+                      const checkChange = (
+                        e: React.SyntheticEvent<HTMLInputElement>,
+                      ) => {
+                        paramsLogic.changeParam(
+                          key,
+                          e.currentTarget.checked ? '2' : '1',
+                        );
+                      };
+                      field = (
+                        <input
+                          type="checkbox"
+                          checked={params[key] === '2'}
+                          onChange={checkChange}
+                        />
+                      );
+                    } else if (obj.type === 'integer') {
+                      const numChange = (
+                        e: React.SyntheticEvent<HTMLInputElement>,
+                      ) => {
+                        paramsLogic.changeParam(key, e.currentTarget.value);
+                      };
+                      field = (
+                        <input
+                          type="number"
+                          step="1"
+                          min={obj.min}
+                          max={obj.max}
+                          value={params[key]}
+                          onChange={numChange}
+                        />
+                      );
+                    } else if (obj.type === 'string') {
+                      const valChange = (
+                        e: React.SyntheticEvent<HTMLInputElement>,
+                      ) => {
+                        paramsLogic.changeParam(key, e.currentTarget.value);
+                      };
+                      field = (
+                        <input
+                          type="text"
+                          value={params[key]}
+                          onChange={valChange}
+                        />
+                      );
+                    } else {
+                      return null;
+                    }
+                  }
+                  return (
+                    <FormField key={key} name={description}>
+                      {field}
+                    </FormField>
                   );
-                } else if (obj.type === 'boolean') {
-                  const checkChange = (
-                    e: React.SyntheticEvent<HTMLInputElement>,
-                  ) => {
-                    paramsLogic.changeParam(
-                      key,
-                      e.currentTarget.checked ? '1' : '2',
-                    );
-                  };
-                  field = (
-                    <input
-                      type="checkbox"
-                      checked={params[key] === '1'}
-                      onChange={checkChange}
-                    />
-                  );
-                } else if (obj.type === 'boolean-reversed') {
-                  const checkChange = (
-                    e: React.SyntheticEvent<HTMLInputElement>,
-                  ) => {
-                    paramsLogic.changeParam(
-                      key,
-                      e.currentTarget.checked ? '2' : '1',
-                    );
-                  };
-                  field = (
-                    <input
-                      type="checkbox"
-                      checked={params[key] === '2'}
-                      onChange={checkChange}
-                    />
-                  );
-                } else if (obj.type === 'integer') {
-                  const numChange = (
-                    e: React.SyntheticEvent<HTMLInputElement>,
-                  ) => {
-                    paramsLogic.changeParam(key, e.currentTarget.value);
-                  };
-                  field = (
-                    <input
-                      type="number"
-                      step="1"
-                      min={obj.min}
-                      max={obj.max}
-                      value={params[key]}
-                      onChange={numChange}
-                    />
-                  );
-                } else if (obj.type === 'string') {
-                  const valChange = (
-                    e: React.SyntheticEvent<HTMLInputElement>,
-                  ) => {
-                    paramsLogic.changeParam(key, e.currentTarget.value);
-                  };
-                  field = (
-                    <input
-                      type="text"
-                      value={params[key]}
-                      onChange={valChange}
-                    />
-                  );
-                } else {
-                  return null;
-                }
-              }
-              return (
-                <FormField key={key} name={description}>
-                  {field}
-                </FormField>
-              );
-            })}
-          </FormControls>
-        </div>
-      </>
+                })}
+              </FormControls>
+            </div>
+          </>
+        )}
+      </ThemeConsumer>
     );
   }
 }
